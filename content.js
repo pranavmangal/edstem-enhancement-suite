947daa6fd1e8c6bcfdb7f80582a4168817869e02
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2021-12-14 23:11:27 +0000

    Update CSS selectors to work with new class names

--- content.js
+++ content.js
@@ -15,9 +15,12 @@ const codeToNameMap = {
   "COMP 70068": "Scheduling & Resource Alloc",
 };
 
+const expandButtonSelector = "span.sbi-content";
+const courseTabSelector = "span.sbi-content";
+
 const buttonObserver = new MutationObserver(function (mutations, observer) {
   for (const _ of mutations) {
-    for (let tab of document.querySelectorAll("span.dsbi-content")) {
+    for (let tab of document.querySelectorAll(expandButtonSelector)) {
       if (tab.innerText.includes("more")) {
         // Expand to show all courses
         tab.click();
@@ -30,8 +33,8 @@ const buttonObserver = new MutationObserver(function (mutations, observer) {
 const coursesObserver = new MutationObserver(function (mutations) {
   for (const _ of mutations) {
     // Replace course codes with names in the sidebar
-    if (document.querySelector("span.dsbi-content")) {
-      Object.values(document.querySelectorAll("span.dsbi-content")).map(
+    if (document.querySelector(courseTabSelector)) {
+      Object.values(document.querySelectorAll(courseTabSelector)).map(
         (course) => {
           if (course.innerText in codeToNameMap) {
             course.innerText = codeToNameMap[course.innerText];
