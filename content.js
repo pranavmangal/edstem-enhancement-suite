1e9aaacf42bd27f20bd792e736dd3ac7af026f36
Hasan Ali <hasan@hasali.dev>
2022-01-23 13:13:48 +0000

    Update for new course naming convention
    - Also display small course codes

--- content.js
+++ content.js
@@ -1,34 +1,3 @@
-const codeToNameMap = {
-  "COMP 60001": "Advanced Architecture",
-  "COMP 60008": "Custom Computing",
-  "COMP 60020": "Simulation and Modelling",
-  "COMP 70001": "Advanced Computer Graphics",
-  "COMP 70004": "Advanced Computer Security",
-  "COMP 70005": "Complexity",
-  "COMP 70006": "Computational Finance",
-  "COMP 70007": "Computational Optimization",
-  "COMP 70008": "Concurrent Processes",
-  "COMP 70009": "Cryptography Engineering",
-  "COMP 70010": "Deep Learning",
-  "COMP 70011": "Individual Project MEng",
-  "COMP 70015": "Mathematics for ML",
-  "COMP 70016": "Natural Language Processing",
-  "COMP 70017": "Principles of Distributed Ledgers",
-  "COMP 70018": "Privacy Engineering",
-  "COMP 70019": "Probablistic Inference",
-  "COMP 70020": "Program Analysis",
-  "COMP 70022": "Scalable Systems and Data",
-  "COMP 70023": "Scalable Software Verification",
-  "COMP 70024": "Software Reliability",
-  "COMP 70025": "Software Engineering for Industry",
-  "COMP 70028": "Reinforcement Learning",
-  "COMP 70030": "Knowledge Representation",
-  "COMP 70031": "Modal Logic for Strategic Reasoning in AI",
-  "COMP 70066": "Decentralised Finance",
-  "COMP 70067": "Robot Learning and Control",
-  "COMP 70068": "Scheduling & Resource Alloc",
-};
-
 const expandButtonSelector = "span.sbi-content";
 const courseTabSelector = "span.sbi-content";
 
@@ -50,8 +19,9 @@ const coursesObserver = new MutationObserver(function (mutations) {
     if (document.querySelector(courseTabSelector)) {
       Object.values(document.querySelectorAll(courseTabSelector)).map(
         (course) => {
-          if (course.innerText in codeToNameMap) {
-            course.innerText = codeToNameMap[course.innerText];
+          const [courseCode, courseName] = course.innerText.split(": ");
+          if (courseName) {
+            course.innerHTML = `${courseName} <small style="opacity: 0.5">(${courseCode.replace("COMP ", "")})</small>`;
           }
         }
       );
